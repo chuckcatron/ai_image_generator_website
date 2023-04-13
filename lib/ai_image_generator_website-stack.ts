cc3a6c7954864b207476833af99a090d09b19ffb
Chuck Catron <charles.catron@gmail.com>
2023-04-13 10:22:49 -0400

    first commit

--- lib/ai_image_generator_website-stack.ts
+++ lib/ai_image_generator_website-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 
 export class AiImageGeneratorWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      bucketName: "react-app-static-website-bucket-thechuckgroup-com",
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AiImageGeneratorWebsiteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3deploy.BucketDeployment(this, "BucketDeployment", {
+      sources: [s3deploy.Source.asset("./website/")],
+      destinationBucket: websiteBucket,
+    });
   }
 }
